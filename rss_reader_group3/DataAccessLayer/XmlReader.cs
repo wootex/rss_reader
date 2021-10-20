07c3d4441e494181cdef517731e1cedbc322bc5b
André Rehnström <rehnstrom.andre@gmail.com>
2021-10-20 11:31:05 +0200

    Update XmlReader.cs

--- rss_reader_group3/DataAccessLayer/XmlReader.cs
+++ rss_reader_group3/DataAccessLayer/XmlReader.cs
@@ -4,10 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.ServiceModel.Syndication;
-
+using System.SyndicationFeed;
+using System.Xml;
 namespace rss_reader_group3
 {
     class XmlReader
     {
+        
+        private void getURL(String url)
+        {
+            XmlReaderSettings rSettings = new XmlReaderSettings();
+            System.Xml.XmlReader.Create("https://www.espn.com/espn/rss/news", rSettings);
+            SyndicationFeed feed = new SyndicationFeed("Feed Title", "Feed Description", new Uri("http://Feed/Alternate/Link"), "FeedID", DateTime.Now);
+
+
+        }
+
+       
     }
 }
