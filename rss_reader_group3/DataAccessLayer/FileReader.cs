6a5a0b13cb8c4ac492a83de9b7efe07e150ddb48
wootex <76428102+wootex@users.noreply.github.com>
2021-10-20 14:40:26 +0200

    h

--- rss_reader_group3/DataAccessLayer/FileReader.cs
+++ rss_reader_group3/DataAccessLayer/FileReader.cs
@@ -5,17 +5,33 @@ using System.Text;
 using System.Threading.Tasks;
 using System.ServiceModel.Syndication;
 using System.Xml;
-namespace rss_reader_group3
+
+namespace DataAccessLayer
 {
-    class XmlReader
+    public class FileReader
     {
         
-        private void getURL(String url)
+        public static void getURL(String url)
         {
-            
-            XmlReader reader = XmlReader.Create("https://www.espn.com/espn/rss/news");
-            SyndicationFeed feed = new SyndicationFeed("Feed Title", "Feed Description", new Uri("http://Feed/Alternate/Link"), "FeedID", DateTime.Now);
+            //https://www.espn.com/espn/rss/news
+            XmlReader reader = XmlReader.Create(url);
+            SyndicationFeed feed = SyndicationFeed.Load(reader);
 
+            Console.WriteLine("---Title: " + feed.Title.Text);
+            Console.WriteLine("---Description: " + feed.Description.Text);
+
+            //foreach (SyndicationItem item in feed.Items)
+            //{
+            //    Console.WriteLine(item.Title.Text);
+            //    Console.WriteLine("->" + item.Summary.Text);
+            //}
+            while (reader.Read())
+            {
+                if ((reader.NodeType == XmlNodeType.Element) && (reader.Name == "Title"))
+                {
+                    if (reader.HasAttributes) ;
+                }
+            }
 
         }
 
